af3cef28941380e22d41477b5c41c3e95e593464
dastonnazarov <dastonnazarov0910@gmail.com>
2023-05-18 09:57:10 +0500

    tag commits

--- src/main/java/com/example/youtube/controller/PlayListController.java
+++ src/main/java/com/example/youtube/controller/PlayListController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/playList")
 @AllArgsConstructor
 public class PlayListController {
+
     private final PlayListService playListService;
     @PostMapping("/private/create")
     public ResponseEntity<PlaylistDTO> create(@RequestBody PlaylistDTO dto){
