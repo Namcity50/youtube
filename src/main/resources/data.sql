c798a2290473d403cb0fe59c673cdaf322cd2e85
Elmurod2203 <saydaliyevelmurod01@gmail.com>
2023-05-22 11:38:20 +0500

    errors have been corrected

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-
-
--- insert into profile(name, surname, email,password,status,role,visible,createdDate,attachEntity,photoId)
--- values ('admin', 'adminov', 'admin@gail.com','11111','ROLE_ADMIN',true,'2023-05-20 18:33:50.690988','1');
